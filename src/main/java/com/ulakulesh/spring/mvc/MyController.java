af040f51131b8bdd0eff0ec77e4fba3e55d22c18
yulia <yulka_kirichok@mail.ru>
2021-11-18 00:33:11 +0300

    added forms Spring MVC to ask-emp-details-view.jsp, edited showEmpDetails method in MyController.java, added class Employee

--- src/main/java/com/ulakulesh/spring/mvc/MyController.java
+++ src/main/java/com/ulakulesh/spring/mvc/MyController.java
@@ -2,36 +2,32 @@ package com.ulakulesh.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
     @RequestMapping ("/")
     public String showFirstView(){
         return "first-view";
     }
     @RequestMapping("/askDetails")
-    public String askEmployeeDetails(){
+    public String askEmployeeDetails(Model model){
+        model.addAttribute("employee", new Employee());
         return "ask-emp-details-view";
     }
-//    @RequestMapping("/showDetails")
-//    public String showEmpDetails(){
-//        return "show-emp-details-view";
-//    }
-//    @RequestMapping("/showDetails")
-//    public String showEmpDetails(HttpServletRequest request, Model model){
-//        String empName = request.getParameter("employeeName");
-//        empName = "Miss " + empName;
-//        model.addAttribute("nameAttribute", empName);
-//    return "show-emp-details-view";
-//    }
+
     @RequestMapping("/showDetails")
-    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model){
-         empName = "Miss " + empName;
-        model.addAttribute("nameAttribute", empName);
+    public String showEmpDetails(@ModelAttribute("employee")Employee emp){
+        String name =emp.getName();
+        emp.setName("Mr/miss "+ name);
+        String surname = emp.getSurname();
+        emp.setSurname(surname+"!");
+
     return "show-emp-details-view";
     }
 
